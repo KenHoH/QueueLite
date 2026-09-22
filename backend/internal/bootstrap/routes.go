dd5752e5dee0554fdba47048a4b5c42d9e5dfeb7
kenhoh <nethk79@gmail.com>
2026-09-23 02:20:24 +0700

    feat(routes): adding all routes

--- backend/internal/bootstrap/routes.go
+++ backend/internal/bootstrap/routes.go
@@ -1,6 +1,18 @@
 package bootstrap
 
 import (
+	businessapp "QueueLite/internal/business/app"
+	businesshttp "QueueLite/internal/business/inbound/http"
+	businessoutbound "QueueLite/internal/business/outbound"
+	counterapp "QueueLite/internal/counter/app"
+	counterhttp "QueueLite/internal/counter/inbound/http"
+	counteroutbound "QueueLite/internal/counter/outbound"
+	queueapp "QueueLite/internal/queue/app"
+	queuehttp "QueueLite/internal/queue/inbound/http"
+	queueoutbound "QueueLite/internal/queue/outbound"
+	subscriptionapp "QueueLite/internal/subscription/app"
+	subscriptionhttp "QueueLite/internal/subscription/inbound/http"
+	subscriptionoutbound "QueueLite/internal/subscription/outbound"
 	userapp "QueueLite/internal/user/app"
 	userhttp "QueueLite/internal/user/inbound/http"
 	useroutbound "QueueLite/internal/user/outbound"
@@ -16,7 +28,27 @@ func NewRouter(db *gorm.DB) *chi.Mux {
 	userService := userapp.NewUserService(userRepo)
 	userHandler := userhttp.NewUserHandler(userService)
 
+	businessRepo := businessoutbound.NewBusinessRepo(db)
+	businessService := businessapp.NewBusinessService(businessRepo)
+	businessHandler := businesshttp.NewBusinessHandler(businessService)
+
+	queueRepo := queueoutbound.NewQueueRepo(db)
+	queueService := queueapp.NewQueueService(queueRepo)
+	queueHandler := queuehttp.NewQueueHandler(queueService)
+
+	counterRepo := counteroutbound.NewCounterRepo(db)
+	counterService := counterapp.NewCounterService(counterRepo, queueRepo)
+	counterHandler := counterhttp.NewCounterHandler(counterService)
+
+	subscriptionRepo := subscriptionoutbound.NewSubscriptionRepo(db)
+	subscriptionService := subscriptionapp.NewSubscriptionService(subscriptionRepo)
+	subscriptionHandler := subscriptionhttp.NewSubscriptionHandler(subscriptionService)
+
 	router.Mount("/users", userHandler.Routes())
+	router.Mount("/businesses", businessHandler.Routes())
+	router.Mount("/queues", queueHandler.Routes())
+	router.Mount("/counters", counterHandler.Routes())
+	router.Mount("/subscriptions", subscriptionHandler.Routes())
 
 	return router
 }
